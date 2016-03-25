dc781abb55ff3f080c2d10f4850e6b54ba8e26d3
zyz <974952098@qq.com>
2016-03-25 10:13:59 +0800

    Initial commit

---
+++ app/src/main/java/net/devwiki/multibuild/Constant.java
@@ -0,0 +1,11 @@
+package net.devwiki.multibuild;
+
+/**
+ * Created by zyz on 2016/3/25.
+ */
+public interface Constant {
+
+    String DEFAULT_URL = "normal";
+
+    String H5_URL = BuildConfig.url + "/h5";
+}
---
+++ app/src/main/java/net/devwiki/multibuild/MainActivity.java
@@ -0,0 +1,19 @@
+package net.devwiki.multibuild;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.text_view);
+        if (textView != null) {
+            textView.setText(Constant.H5_URL);
+        }
+    }
+}
